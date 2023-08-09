70a7c7cd386b8a104ee9fa964d35e02951755ba1
aop-dev <40610962+aop-dev@users.noreply.github.com>
2023-08-09 11:47:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ tg.MainButton.setText("Відправити дані для обробки");
 tg.MainButton.show();
 
 const formData = {
-        "firstname": this.firstName.value,
-        "lastname": this.lastName.value,
-        "middlename": this.middleName.value,
-        "code": this.code.value};
+        "firstname": "this.firstName.value",
+        "lastname": "this.lastName.value",
+        "middlename": "this.middleName.value",
+        "code": "this.code.value"};
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
     tg.sendData(formData);
